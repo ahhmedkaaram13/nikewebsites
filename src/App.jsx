0ac2f2c43e0408aae3bf358ccd59465fbabdcbb2
ahhmedkaaram13 <143730271+ahhmedkaaram13@users.noreply.github.com>
2023-12-12 23:33:20 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import {
+  Hero, PopularProducts, CustomerReviews, Footer, Services, SpecialOffers, Subscribe, SuperQuality
+} from './sections';
+
+const App = () => (
+  <main className="relative">
+    {/* <Nav /> */}
+    <section className="xl:padding-1 
+    wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>   
+    <section className="padding-x py-10">
+      <Services />
+    </section>   
+    <section className="padding">
+      <SpecialOffers />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-12 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
+
+
+export default App;
